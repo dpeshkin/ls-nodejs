94ea3b79f14870e2367973c1fecfd42152ca09cf
DmitryPeshkin <Dmitrey04@mail.ru>
2018-03-10 15:26:51 +0500

    task done

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 const fs = require('fs');
 const path = require('path');
+const args = require('yargs')
+  .usage('Usage: $0 [-sourceDir "directoryName"] [-targetDir "directoryName"] [--clean]')
+  .option('sourceDir', {
+    alias: 'S',
+    describe: 'Source directory name',
+    type: 'string'
+  })
+  .option('targetDir', {
+    alias: 'T',
+    describe: 'Output directory name',
+    type: 'string'
+  })
+  .option('clean', { describe: 'Clean source folder', type: 'boolean' })
+  .help('?')
+  .alias('?', 'help')
+  .example('$0 -D initialFolder -T targetFolder --clean')
+  .demandOption(
+    ['sourceDir', 'targetDir'],
+    'Необходимо указать имена исходной и целевой директорий'
+  ).argv;
 
-const sourceDirectory = path.join(__dirname, 'initialFolder');
-const targetDirectory = path.join(__dirname, 'finalFolder');
+const sourceDirectory = path.normalize(path.join(__dirname, args.sourceDir));
+const targetDirectory = path.normalize(path.join(__dirname, args.targetDir));
 let files = {};
+
+if (!fs.existsSync(sourceDirectory)) {
+  console.log('Упс! Такой папки нет :(');
+  process.exit(1);
+}
 
 const findFiles = directory => {
   fs.readdirSync(directory).forEach(item => {
@@ -21,14 +46,24 @@ const moveFilesToDirectory = (targetDirectory, files) => {
     if (!fs.existsSync(targetDirectory)) fs.mkdirSync(targetDirectory);
     if (!fs.existsSync(fullPath)) fs.mkdirSync(fullPath);
     try {
-      fs
-        .createReadStream(files[file])
-        .pipe(fs.createWriteStream(path.join(fullPath, file)));
+      fs.createReadStream(files[file]).pipe(fs.createWriteStream(path.join(fullPath, file)));
     } catch (error) {
       console.log(error);
     }
   }
 };
 
+const cleanFolder = directory => {
+  if (fs.existsSync(directory)) {
+    fs.readdirSync(directory).forEach(item => {
+      const itemPath = path.join(directory, item);
+      const itemIsDirectory = fs.statSync(itemPath).isDirectory();
+      itemIsDirectory ? cleanFolder(itemPath) : fs.unlinkSync(itemPath);
+    });
+    fs.rmdirSync(directory);
+  }
+};
+
 findFiles(sourceDirectory);
 moveFilesToDirectory(targetDirectory, files);
+if (args.clean) cleanFolder(sourceDirectory);
