ff51e12865d49b1954a9615f31e69096a67962aa
DmitryPeshkin <Dmitrey04@mail.ru>
2018-03-10 10:18:19 +0500

    refactored app.js

--- app.js
+++ app.js
@@ -1,35 +1,34 @@
 const fs = require('fs');
 const path = require('path');
 
-const sourceDir = path.join(__dirname, 'initialFolder');
-const targetDir = path.join(__dirname, 'finalFolder');
-
-const moveFileToNewDirectory = (targetDir, itemPath, item) => {
-  const dirName = item[0].toUpperCase();
-  const fullPath = path.join(targetDir, dirName);
-  if (!fs.existsSync(targetDir)) fs.mkdirSync(targetDir);
-  if (!fs.existsSync(fullPath)) fs.mkdirSync(fullPath);
-  try {
-    fs
-      .createReadStream(itemPath)
-      .pipe(fs.createWriteStream(path.join(fullPath, item)));
-  } catch (error) {
-    console.log(error);
-  }
-};
+const sourceDirectory = path.join(__dirname, 'initialFolder');
+const targetDirectory = path.join(__dirname, 'finalFolder');
+let files = {};
 
 const findFiles = directory => {
   fs.readdirSync(directory).forEach(item => {
-    if (item === '.DS_Store') return;
-
+    if (item === '.DS_Store') return null;
     const itemPath = path.join(directory, item);
     const itemIsDirectory = fs.statSync(itemPath).isDirectory();
-    if (itemIsDirectory) {
-      findFiles(itemPath);
-      return;
-    }
-    moveFileToNewDirectory(targetDir, itemPath, item);
+    itemIsDirectory ? findFiles(itemPath) : (files[item] = itemPath);
   });
+}; // собирает файлы в словарь {fileName: filePath}
+
+const moveFilesToDirectory = (targetDirectory, files) => {
+  for (let file in files) {
+    const newDirectoryName = file[0].toUpperCase();
+    const fullPath = path.join(targetDirectory, newDirectoryName);
+    if (!fs.existsSync(targetDirectory)) fs.mkdirSync(targetDirectory);
+    if (!fs.existsSync(fullPath)) fs.mkdirSync(fullPath);
+    try {
+      fs
+        .createReadStream(files[file])
+        .pipe(fs.createWriteStream(path.join(fullPath, file)));
+    } catch (error) {
+      console.log(error);
+    }
+  }
 };
 
-findFiles(sourceDir);
+findFiles(sourceDirectory);
+moveFilesToDirectory(targetDirectory, files);
